1b714970a55cceb63b82153d17b10591937892ab
Rafael Freitas <rafaelborges1985@hotmail.com>
2019-10-30 14:24:23 +0000

    created arrows for image layer

--- development/scripts/bundle.js
+++ development/scripts/bundle.js
@@ -113,17 +113,9 @@ const Layer = {
         if(e.target.nodeName === 'IMG'){
             // $('.layer-closed').addClass('layer').removeClass('layer-closed');
             $('.layer').toggleClass('hidden');
-            $('.layer').html('<img class="large-image"/><div class="close-btn">X</div>');
+            $('.layer').html('<img class="large-image"/><div class="left-arrow"><i class="fas fa-chevron-left"></i></div><div class="right-arrow"><i class="fas fa-chevron-right"></i></div><div class="close-btn">X</div>');
             
             document.querySelector('.large-image').src = e.target.src;
-            
-            if($('.large-image').width() > $(window).width()) {
-                let ratio = $('.large-image').width - $(window).width();
-            } else if ($('.large-image').height() == $('.large-image').height()) {
-                console.log('aia');
-            }
-            
-            
             
             $('.layer .close-btn').on('click', function(){
                 // $('.layer').addClass('layer-closed').removeClass('layer');
