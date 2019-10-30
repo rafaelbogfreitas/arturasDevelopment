ab431fde7f0a04a70bd54d1e9a80e449370f6688
Rafael Freitas <rafaelborges1985@hotmail.com>
2019-10-30 16:11:11 +0000

    finished carousel for images

--- components/scripts/layer.js
+++ components/scripts/layer.js
@@ -3,20 +3,63 @@ const $ = require('jquery');
 const Layer = {
 
     imageLayer(e) {
-        console.log(e);
+        //takes the event target and checks if it is an image
         if(e.target.nodeName === 'IMG'){
-            // $('.layer-closed').addClass('layer').removeClass('layer-closed');
+            //if image, removes class hidden to reveal the layer with the img clicked
             $('.layer').toggleClass('hidden');
-            $('.layer').html('<img class="large-image"/><div class="left-arrow"><i class="fas fa-chevron-left"></i></div><div class="right-arrow"><i class="fas fa-chevron-right"></i></div><div class="close-btn">X</div>');
+            $('.layer').html('<img class="large-image animated fadeIn"/><div class="left-arrow"><i class="fas fa-chevron-left"></i></div><div class="right-arrow"><i class="fas fa-chevron-right"></i></div><div class="close-btn">X</div>');
             
-            document.querySelector('.large-image').src = e.target.src;
+            //uses the source of the image clicked
+            let imgSrc = e.target.src;
+            document.querySelector('.large-image').src = imgSrc; 
             
+            //left arrow click event to change pics
+            if($('.left-arrow')){
+                $('.left-arrow').on('click', function(){
+                    let imgArr = document.querySelectorAll('#images-container img');
+                    console.log(imgArr);
+                    for(let i = 0; i < imgArr.length; i++){
+                        if(imgArr[i].src === imgSrc && i !== 0 ){
+                            imgSrc = imgArr[i-1].src;
+                            $('.large-image').remove()
+                            $('.layer').prepend('<img class="large-image animated fadeIn"/>');
+                            document.querySelector('.large-image').src = imgSrc; 
+                        } else if (imgArr[i].src === imgSrc && i === 0) {
+                            imgSrc = imgArr[imgArr.length - 1].src;
+                            $('.large-image').remove()
+                            $('.layer').prepend('<img class="large-image animated fadeIn"/>');
+                            document.querySelector('.large-image').src = imgSrc;
+                        }
+                    }
+                });
+            }
+            
+            if($('.right-arrow')){
+                $('.right-arrow').on('click', function(){
+                    let imgArr = document.querySelectorAll('#images-container img');
+                    console.log(imgArr.length);
+                    for(let i = 0; i < imgArr.length; i++){
+                        if(imgArr[i].src === imgSrc && i !== imgArr.length - 1){
+                            imgSrc = imgArr[i+1].src;
+                            $('.large-image').remove()
+                            $('.layer').prepend('<img class="large-image animated fadeIn"/>');
+                            return document.querySelector('.large-image').src = imgSrc; 
+                        } else if (imgArr[i].src === imgSrc && i === imgArr.length - 1) {
+                            imgSrc = imgArr[0].src;
+                            $('.large-image').remove()
+                            $('.layer').prepend('<img class="large-image animated fadeIn"/>');
+                            return document.querySelector('.large-image').src = imgSrc;
+                        }
+                    }
+                });
+            }
+            
+            
+
             $('.layer .close-btn').on('click', function(){
-                // $('.layer').addClass('layer-closed').removeClass('layer');
                 $('.layer').toggleClass('hidden');
                 $('.layer-closed .close-btn').remove();
                 $('.large-image').remove();
-
             });
         }
     }
